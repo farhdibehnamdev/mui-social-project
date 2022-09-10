93ec218a80f1152243273f9e2c4122c20c963adc
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-09-10 16:55:49 +0430

    💄 Added userBox and Icons to Navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,19 @@
-import { AppBar, styled, Toolbar, Typography } from "@mui/material";
-import { Pets } from "@mui/icons-material";
-import { borderRadius } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
+import {
+  AppBar,
+  styled,
+  Toolbar,
+  Typography,
+  Box,
+  Input,
+  Badge,
+  InputBase,
+  Avatar,
+  MenuItem,
+  Menu,
+} from "@mui/material";
+import { Pets, Mail, Notifications } from "@mui/icons-material";
+
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
@@ -11,18 +23,67 @@ const Search = styled("div")(({ theme }) => ({
   backgroundColor: "white",
   padding: "0 10px",
   borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
 }));
 
 const Navbar = function () {
+  const [open, setOpen] = useState(false);
   return (
     <AppBar position="sticky">
       <StyledToolbar>
         <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
           Lama Dev
         </Typography>
-        <Pets sx={{ display: { xs: "block", sm: "block" } }} />
-        <Search> </Search>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar sx={{ width: 30, height: 30 }} src="" />
+        </Icons>
+        <UserBox onClick={(e) => setOpen(true)}>
+          <Avatar sx={{ width: 30, height: 30 }} src="" />
+          <Typography variant="span">John</Typography>
+        </UserBox>
       </StyledToolbar>
+      <Menu
+        id="fade-menu"
+        MenuListProps={{
+          "aria-labelledby": "fade-button",
+        }}
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
